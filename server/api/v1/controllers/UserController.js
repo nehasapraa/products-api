a61d1ac2d100b0deec64fa14ce2bf05600c92dd6
nehasapraa <neha.sapraa@gmail.com>
2020-05-28 00:55:50 +1000

    lint issues

--- server/api/v1/controllers/UserController.js
+++ server/api/v1/controllers/UserController.js
@@ -2,10 +2,10 @@ import UserService from '../services/user.service';
 export class UserController {
   user(req, res) {
     UserService.get_user()
-    .then(r => res.send(r))
-    .catch(e => {
-          res.send(`Status ${e.response.status} ${e.response.data}`)
-    });
+      .then(r => res.send(r))
+      .catch(e => {
+        res.send(`Status ${e.response.status} ${e.response.data}`);
+      });
   }
 }
 export default new UserController();
