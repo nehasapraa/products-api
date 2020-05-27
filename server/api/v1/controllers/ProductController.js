dc5622d81d48d26d4dde9ce5c7a658db8b816121
nehasapraa <neha.sapraa@gmail.com>
2020-05-28 00:53:13 +1000

    error handling

--- server/api/v1/controllers/ProductController.js
+++ server/api/v1/controllers/ProductController.js
@@ -3,15 +3,25 @@ import SortService from '../services/sort.service';
 
 export class ProductController {
   all(req, res) {
-    ProductService.all().then(r => res.json(r.data));
+    ProductService.all()
+    .then(r => res.json(r.data))
+    .catch(e => {
+      res.send(`Status ${e.response.status} ${e.response.data}`)
+    });
   }
   shopping_history(req, res) {
-    ProductService.get_customer_data().then(r => res.json(r.data));
+    ProductService.get_customer_data()
+    .then(r => res.json(r.data))
+    .catch(e => {
+          res.send(`Status ${e.response.status} ${e.response.data}`)
+    });
   }
   sort(req, res) {
-    ProductService.get_customer_data().then(async response => {
+    ProductService.get_customer_data()
+    .then(async response => {
       const customers = response.data;
-      ProductService.all().then(async r => {
+      ProductService.all()
+      .then(async r => {
         let products =
           (req.query.sortOption.toLowerCase() === 'recommended')
             ? await SortService.getPopularProducts(customers, r.data)
@@ -19,7 +29,13 @@ export class ProductController {
         products.sort(SortService.dynamicSort(req.query.sortOption));
         products = await ProductService.delete_unwanted_fields(products);
         res.json(products);
+      })
+      .catch(e => {
+            res.send(`Status ${e.response.status} ${e.response.data}`)
       });
+    })
+    .catch(e => {
+          res.send(`Status ${e.response.status} ${e.response.data}`)
     });
   }
 }
--- server/api/v1/controllers/UserController.js
+++ server/api/v1/controllers/UserController.js
@@ -1,7 +1,11 @@
 import UserService from '../services/user.service';
 export class UserController {
   user(req, res) {
-    UserService.get_user().then(r => res.send(r));
+    UserService.get_user()
+    .then(r => res.send(r))
+    .catch(e => {
+          res.send(`Status ${e.response.status} ${e.response.data}`)
+    });
   }
 }
 export default new UserController();
